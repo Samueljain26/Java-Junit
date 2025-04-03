13d1c4b19fb142cefa0f17ac83da62e18876c204
Samueljain26 <samueljain77@gmail.com>
2025-04-03 15:44:00 +0530

    [Samuel] ADD: Testing Long running task that it fails after running for 2 seconds.

--- src/test/java/com/bridgelabz/LongRunningTaskTest.java
+++ src/test/java/com/bridgelabz/LongRunningTaskTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 public class LongRunningTaskTest {
 
     @Test
-    @Timeout(value =4, unit = TimeUnit.SECONDS)
+    @Timeout(value =2, unit = TimeUnit.SECONDS)
     void shouldLongRunningTask() throws InterruptedException {
         String result = LongRunningTask.longRunningTask();
         Assertions.assertEquals("Done", result);
